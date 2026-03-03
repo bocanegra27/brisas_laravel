2d194787df7900ad776e91b87976d71ddbebebab
Natalia <natalia.qek@gmail.com>
2026-03-02 20:51:56 -0500

    .

--- resources/views/personalizar.blade.php
+++ resources/views/personalizar.blade.php
@@ -197,7 +197,7 @@
                         </div>
                     @endforeach
 
-                    <button type="submit" class="btn btn-primary w-100 py-3 fw-bold text-uppercase mt-4">Guardar Diseño</button>
+                    <button type="submit" class="btn btn-primary w-100 py-3 fw-bold text-uppercase mt-4">Contactar para personalizar.</button>
                 </form>
             </div>
         </div>
